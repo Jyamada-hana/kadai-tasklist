51e4de7d5a7789b50a5dd0d69c925aafa482c8b4
mint_hana.mama <mint_hana_mama@yahoo.co.jp>
2020-03-20 01:12:24 +0000

    add column status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,12 +48,14 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
 
-        $message = new Task;
-        $message->content = $request->content;
-        $message->save();
+        $task = new Task;
+        $task->status = $request->status;    // 追加
+        $task->content = $request->content;
+        $task->save();
 
         return redirect('/');
     }
@@ -102,10 +104,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $task = Task::find($id);
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
